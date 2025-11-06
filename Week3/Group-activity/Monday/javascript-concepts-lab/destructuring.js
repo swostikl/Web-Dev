7441517b829fc5b8a12b03c2ab457a8b4f4fd9f2
Swostika Lama <Swostika.Lama@metropolia.fi>
2025-11-06 06:05:36 +0200

    Applied Object destructuring method

---
+++ Week3/Group-activity/Monday/javascript-concepts-lab/destructuring.js
@@ -0,0 +1,22 @@
+// const person = {name : 'Alice', age:30, city : 'New York'};
+// const {name, age} = person;
+
+// console.log('Name : ', name);
+// console.log('age : ', age);
+
+
+//Optional: Example 1 - Extracting Nested Properties:
+// const person = { name: 'Alice', info: { age: 30, occupation: 'Engineer' } };
+
+// const { name, info: { age, occupation } } = person;
+
+// console.log('Name:', name);
+// console.log('Age:', age);
+// console.log('Occupation:', occupation);
+
+
+function greetUser({ name, age }) {
+  console.log(`Hello, ${name}! You're ${age} years old.`);
+}
+
+greetUser({ name: 'Bob', age: 25 });
