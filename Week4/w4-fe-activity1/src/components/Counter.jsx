71b7d6f0265bece21ac3795861fda2da4bdbe7d0
Swostika Lama <Swostika.Lama@metropolia.fi>
2025-11-10 10:44:53 +0200

    creates state using useState hook and use onClick eventlistener to add some functionality

---
+++ Week4/w4-fe-activity1/src/components/Counter.jsx
@@ -0,0 +1,36 @@
+import './Counter.css';
+
+import { useState } from 'react'; 
+
+const Counter = () => {
+  const [theme, setTheme] = useState('light');
+  const [count, setCount] = useState (0);
+
+  const toggleTheme = ()=>{
+    theme=== 'light' ? setTheme('dark') : setTheme('light')
+  }
+
+  
+
+  return (
+    //<div className="state">
+    <div className={`state ${theme}`}>
+      <h1>UseState Component</h1>
+      <button onClick = {()=> setTheme('dark')}>Dark</button>
+      <button onClick = {()=> setTheme('light')}>Light</button>
+
+      <button onClick = {toggleTheme}>Toggle Theme</button>
+      
+
+      <h2>{count}</h2>
+      <button onClick = {()=>setCount(prevCount => prevCount+1)}>
+        Increment
+      </button>
+      <button onClick= {()=> setCount(prevCount => prevCount - 1)}>
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
