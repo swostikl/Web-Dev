65c252065cdfc1768efca7babbb383331df8471f
Swostika Lama <Swostika.Lama@metropolia.fi>
2025-11-24 10:41:35 +0200

    implement login flow and switch to sessionStorage

---
+++ Week6/react-auth/frontend/src/pages/LoginComponent.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const LoginComponent = ({ setIsAuthenticated }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  // const handleLogin = async () => {
+  //   try {
+	// 	// endpoint: POST /api/user/login
+  //   } catch (error) {
+  //     console.error("Error during login:", error);
+  //   }
+  // };
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("/api/user/Login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const user = await response.json();
+        sessionStorage.setItem("user", JSON.stringify(user));
+        console.log("User log in successfully!");
+        setIsAuthenticated(true);
+        navigate("/");
+      } else {
+        console.error("Login failed");
+      }
+    } catch (error) {
+      console.error("Error during Login:", error);
+    }
+  };
+
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <label>
+        Email:
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <button onClick={handleLogin}>Log In</button>
+    </div>
+  );
+};
+
+export default LoginComponent;
